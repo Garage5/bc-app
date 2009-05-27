601ecfec60e3a7203bcb1a8c79214491376d8787
Bryan Cinman <syngenius@gmail.com>
2009-05-26 22:42:47 -0400

    lite preview

--- app/controllers/preview_controller.rb
+++ app/controllers/preview_controller.rb
@@ -4,4 +4,8 @@ class PreviewController < ApplicationController
     render params[:view].join('/')
   end
   
+  def lite
+    render params[:view].join('/'), :layout => 'lite'
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -39,4 +39,5 @@ ActionController::Routing::Routes.draw do |map|
   # Note: These default routes make all actions in every controller accessible via GET requests. You should
   # consider removing the them or commenting them out if you're using named routes and resources.
   map.connect '/preview/*view', :controller => 'preview', :action => 'index'
+  map.connect '/preview-lite/*view', :controller => 'preview', :action => 'lite'
 end
