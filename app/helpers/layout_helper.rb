5a818ccac4cb410732745ce30ae598b5dfc0828b
Bryan Cinman <syngenius@gmail.com>
2009-05-30 23:22:30 -0400

    started messages page

---
+++ app/helpers/layout_helper.rb
@@ -0,0 +1,27 @@
+# These helper methods can be called in your template to set variables to be used in the layout
+# This module should be included in all views globally,
+# to do so you may need to add this line to your ApplicationController
+#   helper :layout
+module LayoutHelper
+  def title(page_title, show_title = true)
+    @content_for_title = page_title.to_s
+    @show_title = show_title
+  end
+  
+  def body(body_id)
+    content_for(:body) { body_id.to_s }
+  end
+  
+  def show_title?
+    @show_title
+  end
+  
+  def stylesheet(*args)
+    content_for(:head) { stylesheet_link_tag(*args.map(&:to_s)) }
+  end
+  
+  def javascript(*args)
+    args = args.map { |arg| arg == :defaults ? arg : arg.to_s }
+    content_for(:head) { javascript_include_tag(*args) }
+  end
+end
