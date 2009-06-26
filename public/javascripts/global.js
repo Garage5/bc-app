01a34f73d857e2c6c99d29716ae4280e027d4e30
Bryan Cinman <syngenius@gmail.com>
2009-06-26 17:26:35 -0400

    closing notes globalified, made button.btn display inline

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -2,6 +2,6 @@ $(document).ready(function(){
   // hide .note elements when closed
   $(".note-close").click(function(e){
     e.preventDefault()
-    $(this).parent().hide(400)
+    $(this).closest('.note').hide(400)
   })
 })
