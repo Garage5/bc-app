ba1a338637dd632f5e1c291a8a7c24abd7ba4006
Davi Medrade <davimedrade@gmail.com>
2009-07-31 09:37:17 -0300

    follow-up: error message if the co-host is already hosting the tournament

--- app/controllers/participations_controller.rb
+++ app/controllers/participations_controller.rb
@@ -12,7 +12,9 @@ class ParticipationsController < ApplicationController
     user = User.find(:first, :conditions => ['login = ? OR email = ?', params[:user], params[:user]])
     if user
       participation = user.cohost_tournament @tournament
-      if participation.errors.on(:participant_id)
+      if user.is_hosting?(@tournament)
+        flash[:error] = "This user is already hosting or co-hosting this tournament."
+      elsif user.is_participant_of?(@tournament)
         flash[:error] = "You can not add a participant as a co-host. Please remove the user from participants first."
       end
     else
