2c63ad6550ca73ca106bbf624e460c02825b1a3d
Bryan <bryan@bryans-macbook-pro.local>
2009-08-12 17:20:22 -0400

    fixed match positioning on heroku

--- app/models/round.rb
+++ app/models/round.rb
@@ -2,7 +2,7 @@ class Round < ActiveRecord::Base
   named_scope :by_number, lambda {|n| {:conditions => {:number => n}}}
   
   belongs_to :tournament
-  has_many   :matches, :order => 'position desc'
+  has_many   :matches, :order => 'position asc'
   
   acts_as_list :scope => :tournament
 end
