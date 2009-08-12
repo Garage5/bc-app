938019cfa5bc2b414e985c4e5e40d68c058a91b8
Bryan <bryan@bryans-macbook-pro.local>
2009-08-12 17:19:26 -0400

    still trying to debug match positioning on heroku

--- app/models/round.rb
+++ app/models/round.rb
@@ -2,7 +2,7 @@ class Round < ActiveRecord::Base
   named_scope :by_number, lambda {|n| {:conditions => {:number => n}}}
   
   belongs_to :tournament
-  has_many   :matches
+  has_many   :matches, :order => 'position desc'
   
   acts_as_list :scope => :tournament
 end
