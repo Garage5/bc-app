ec13a749ef92d924013e91cd1caafd25ea972646
Bryan <bryan@bryans-macbook-pro.local>
2009-08-21 16:52:57 -0400

    added bracket specs

--- spec/models/match_spec.rb
+++ spec/models/match_spec.rb
@@ -18,17 +18,16 @@ describe Match do
   it "should advance 1st player of 2nd match to 2nd slot in 1st match in the next round" do
     match = @tournament.rounds.first.matches[1]
     match.slot(0).advance!
-    match.next.slot_two.should == match.slot_one
+    match.next.slot(0).should == match.slot(0)
   end
   
   it "should advance 2nd player of 3nd match to 1nd slot in 2nd match in the next round" do
     match = @tournament.rounds.first.matches[2]
     match.slot(1).advance!
-    match.next.slot_one.should == match.slot_two
+    match.next.slot(0).should == match.slot(1)
   end
   
   it "should advance winner to next round when both players are in agreement" do
-    @tournament.start
     match = @tournament.rounds.first.matches.first
     match.slot(0).won!
     match.slot(1).lost!
@@ -36,13 +35,44 @@ describe Match do
     match.next.slot(0).should == match.slot(1)
   end
   
-  it "should disqualify a slot"
+  it "should disqualify a player" do
+    match = @tournament.rounds.first.matches.first
+    match.slot(1).disqualify!
+    match.slot(1).status.should == 'disqualified'
+    match.winner.should == match.slot(0)
+  end
   
-  it "should advance opponent when slot is disqualified"
+  it "should advance opponent when slot is disqualified" do
+    match = @tournament.rounds.first.matches.first
+    match.slot(1).disqualify!
+    match.winner.should == match.slot(0)
+    match.next.slot(0).should == match.slot(0)
+  end
   
-  it "should revert a slot to previous match"
+  it "should remove player from match when reverted" do
+    match = @tournament.rounds.first.matches.first
+    match.slot(1).advance
+    match.next.slot(0).revert!
+    match.next.slot(0).should be_nil
+  end
   
-  it "should nullify winner of previews match when slot is reverted"
+  it "should nullify winner of previous match when slot is reverted" do
+    match = @tournament.rounds.first.matches.first
+    match.slot(1).advance
+    match.next.slot(0).revert!
+    match.winner.should be_nil
+  end
+  
+  it "should make winner of last match winner of tournament" do
+    @tournament.rounds.each do |r|
+      r.matches.each do |m|
+        m.slot(0).advance!
+      end unless r == @tournamnet.rounds.last
+    end
+    match = @tournament.rounds.last.matches.first
+    match.slot(0).advance!
+    @tournament.winner.should == match.slot(0)
+  end
   
   describe Slot do
     
