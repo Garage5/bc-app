6bc3bb9e37196bfac28a4034d1e49b07ca79c0a7
Josh Kim <joshkim@gmail.com>
2009-08-31 10:18:29 -0400

    Fixing cookies, Rails 2.3 peculiarity

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,4 +17,4 @@ config.action_controller.perform_caching             = false
 config.action_mailer.raise_delivery_errors = true
 
 # Cookies across base URL
-config.action_controller.session[:domain] = '.tbc.heroku.com'
+#config.action_controller.session[:domain] = '.tbc.heroku.com'
