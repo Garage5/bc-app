4df407c2771755da3a81284344d0cc80741a5230
Josh Kim <joshkim@gmail.com>
2009-09-02 18:17:30 -0400

    For the 80th time, now this should work

--- config/initializers/subdomain_config.rb
+++
@@ -1,5 +0,0 @@
-SubdomainFu.tld_sizes = {:development => 1, #if .localhost, set to 0, if asdf.com, then 1
-                         :test => 1, # since rspec
-                         :production => 1}
-
-SubdomainFu.mirrors = ["www"]
