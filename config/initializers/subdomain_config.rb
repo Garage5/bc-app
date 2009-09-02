bc68b6c2c83feadb31910d5bafdcebc3857e702a
Josh Kim <joshkim@gmail.com>
2009-09-02 18:14:07 -0400

    Ahem

--- config/initializers/subdomain_config.rb
+++ config/initializers/subdomain_config.rb
@@ -1,14 +1,5 @@
-<<<<<<< HEAD
 SubdomainFu.tld_sizes = {:development => 1, #if .localhost, set to 0, if asdf.com, then 1
-=======
-SubdomainFu.tld_sizes = {:development => 1, #for .local, if .localhost, set to 0
->>>>>>> 40a1aee... god hates me
                          :test => 1, # since rspec
                          :production => 1}
 
-# These are the subdomains that will be equivalent to no subdomain  
-<<<<<<< HEAD
 SubdomainFu.mirrors = ["www"]
-=======
-SubdomainFu.mirrors = ["www"]
->>>>>>> 40a1aee... god hates me
