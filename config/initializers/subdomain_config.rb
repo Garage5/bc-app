f65a9d6db2ddb7ee42f00c7d3b2275a6c7c420b4
Matt Johnson <mattjohnson@mattjohnson-3.local>
2009-09-02 13:36:47 -0500

    ummm yeah. oops.

---
+++ config/initializers/subdomain_config.rb
@@ -0,0 +1,6 @@
+SubdomainFu.tld_sizes = {:development => 1, #for .local, if .localhost, set to 0
+                         :test => 1, # since rspec
+                         :production => 1}
+
+# These are the subdomains that will be equivalent to no subdomain  
+SubdomainFu.mirrors = ["www"]
