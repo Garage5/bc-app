40a1aee82265f5ceafde418e930d91bb3ec0719c
Bryan <bryan@bryans-macbook-pro.local>
2009-09-02 15:47:37 -0400

    god hates me

--- config/initializers/subdomain_config.rb
+++
@@ -1,6 +0,0 @@
-SubdomainFu.tld_sizes = {:development => 1, #for .local, if .localhost, set to 0
-                         :test => 1, # since rspec
-                         :production => 1}
-
-# These are the subdomains that will be equivalent to no subdomain  
-SubdomainFu.mirrors = ["www"]
