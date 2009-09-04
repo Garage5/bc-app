fffd1c65d85a74d2d8c5a11b5f1e5db6bf815ec7
Josh Kim <joshkim@gmail.com>
2009-09-04 15:00:30 -0400

    Please please please stop it with your subdomain foolishness

---
+++ config/initializers/subdomain_config.rb
@@ -0,0 +1,6 @@
+SubdomainFu.tld_sizes = {:development => 1, #if .localhost, set to 0, if asdf.com, then 1
+                         :test => 1, # since rspec
+                         :production => 1}
+
+# These are the subdomains that will be equivalent to no subdomain  
+SubdomainFu.mirrors = ["www"]
