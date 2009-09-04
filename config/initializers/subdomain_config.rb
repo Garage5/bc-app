aa59a806e72da7edf60dddd464871024dfed2e21
Josh Kim <joshkim@gmail.com>
2009-09-04 15:20:02 -0400

    Using heroku config vars, subdomain will be fixed.

--- config/initializers/subdomain_config.rb
+++ config/initializers/subdomain_config.rb
@@ -1,4 +1,4 @@
-SubdomainFu.tld_sizes = {:development => ENV['SUBDOMAIN'] || 0, #if .localhost, set to 0, if asdf.com, then 1
+SubdomainFu.tld_sizes = {:development => ENV['SUBDOMAIN'].to_i || 0, #if .localhost, set to 0, if asdf.com, then 1
                          :test => 1, # since rspec
                          :production => 1}
 
