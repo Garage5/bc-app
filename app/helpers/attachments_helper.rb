5a25d708ea4aae39998124f462460201797a6472
Bryan <bryan@bryans-macbook-pro.local>
2009-12-08 16:22:50 -0500

    fixed attachment icons 404ing on heroku

--- app/helpers/attachments_helper.rb
+++ app/helpers/attachments_helper.rb
@@ -1,27 +1,27 @@
 module AttachmentsHelper
+ICONS = [
+  'ai', 
+  'bmp', 
+  'css', 
+  'doc', 
+  'gif', 
+  'html', 
+  'jpeg', 
+  'jpg', 
+  'mov', 
+  'mp3', 
+  'pdf', 
+  'php', 
+  'psd', 
+  'rar', 
+  'wmv', 
+  'xml', 
+  'zip'
+]
 
 def icon_url_for_filetype(filename)
-  icons = [
-    'ai', 
-    'bmp', 
-    'css', 
-    'doc', 
-    'gif', 
-    'html', 
-    'jpeg', 
-    'jpg', 
-    'mov', 
-    'mp3', 
-    'pdf', 
-    'php', 
-    'psd', 
-    'rar', 
-    'wmv', 
-    'xml', 
-    'zip'
-  ]
   extension = filename.split('.').last
-  icon = icons.include?(extension) ? "#{extension.capitalize}.png" : "Default.png" 
+  icon = ICONS.include?(extension.downcase) ? "#{extension.upcase}.png" : "Default.png" 
   return "/images/attach_types/#{icon}"
 end
 
