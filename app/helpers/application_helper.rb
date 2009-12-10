d52df1eeda81f9d0977d6c7db2bf5c587ed69f0c
Bryan <bryan@bryans-macbook-pro.local>
2009-12-10 15:54:49 -0500

    relative dates on overview

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -18,5 +18,10 @@ module ApplicationHelper
   def discount_label(discount)
     (discount.percent? ? number_to_percentage(discount.amount * 100, :precision => 0) : number_to_currency(discount.amount)) + ' off'
   end
-
+  
+  def relative_date_or_formatted(date)
+    return 'Today' if date.to_date == Date.today
+    return 'Yesterday' if date.to_date == Date.today - 1
+    return date.strftime('%B %d, %Y')
+  end
 end
