0c6bb91baaf40101f1a21dae2d342d4d48595bc4
Bryan <bryan@bryans-macbook-pro.local>
2009-12-18 23:32:26 -0500

    removed name column from users table

--- db/migrate/20090527184542_create_users.rb
+++ db/migrate/20090527184542_create_users.rb
@@ -2,7 +2,6 @@ class CreateUsers < ActiveRecord::Migration
   def self.up
     create_table :users do |t|
       t.string :login,              :null => false
-      t.string :name,               :null => false
       t.string :email,              :null => false
       t.string :crypted_password,   :null => false
       t.string :password_salt,      :null => false
