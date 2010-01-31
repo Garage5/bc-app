c4eca0df592d104761855f9f1c675ce3237a054c
Bryan <bryan@bryans-macbook-pro.local>
2010-01-31 15:33:24 -0500

    fixed avatar not being saved on dev account as a result of trying to validate the username being atleast 4 characters long [#22 state:resolved]

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@ class User < ActiveRecord::Base
   has_many :teams, :through => :team_members
   
   validates_acceptance_of :terms_of_service, :on => :create
-  validates_length_of :username, :within => 4..13
+  validates_length_of :username, :within => 4..13, :on => :create
   validates_presence_of :username
   
   attr_accessible :avatar, :email, :password, :password_confirmation
