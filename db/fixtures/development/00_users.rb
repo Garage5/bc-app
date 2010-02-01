23371223a28204557c568ad50ef7df2874c25274
Bryan <bryan@bryans-macbook-pro.local>
2010-02-01 14:41:22 -0500

    changed minimum username length to 3

--- db/fixtures/development/00_users.rb
+++ db/fixtures/development/00_users.rb
@@ -1,19 +1,42 @@
-User.seed_many(:username, :email, [
-    ['dev',    'Developer User'],
-    ['bryan',  'Bryan Cinman'],
-    ['matt',   'Matt Johnson'],
-    ['james',  'James Au'],
-    ['victor', 'Victor Au'],
-    ['david',  'David Nguyen'],
-    ['josh',   'Josh Kim'],
-    ['jeff',   'Jeff Tang'],
-    ['jon',    'Jon Tang'],
-    ['john',    'John Smith'],
-    ['jake',    'Jake Sully']
-  ].collect do |u|
-    { :username => u[0], 
-      :email => "#{u[0]}@thebattlebegins.com", 
-      :password => 'pass', 
-      :password_confirmation => 'passpass' }
+# User.seed_many(:username, :email, [
+#     ['dev',    'Developer User'],
+#     ['bryan',  'Bryan Cinman'],
+#     ['matt',   'Matt Johnson'],
+#     ['james',  'James Au'],
+#     ['victor', 'Victor Au'],
+#     ['david',  'David Nguyen'],
+#     ['josh',   'Josh Kim'],
+#     ['jeff',   'Jeff Tang'],
+#     ['jon',    'Jon Tang'],
+#     ['john',    'John Smith'],
+#     ['jake',    'Jake Sully']
+#   ].collect do |u|
+#     { :username => u[0], 
+#       :email => "#{u[0]}@thebattlebegins.com", 
+#       :password => 'pass', 
+#       :password_confirmation => 'pass' }
+#   end
+# )
+
+users = [
+  ['dev',    'Developer User'],
+  ['bryan',  'Bryan Cinman'],
+  ['matt',   'Matt Johnson'],
+  ['james',  'James Au'],
+  ['victor', 'Victor Au'],
+  ['david',  'David Nguyen'],
+  ['josh',   'Josh Kim'],
+  ['jeff',   'Jeff Tang'],
+  ['jon',    'Jon Tang'],
+  ['john',    'John Smith'],
+  ['jake',    'Jake Sully']
+]
+
+users.each do |u|
+  User.create do |u|
+    u.username = u[0]
+    u.email = "#{u[0]}@thebattlebegins.com"
+    u.password = 'pass'
+    u.password_confirmation = 'pass'
   end
-)
+end
