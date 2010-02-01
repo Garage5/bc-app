71aca8f76cb4db639f0bc8120d0791b403011bdb
Bryan <bryan@bryans-macbook-pro.local>
2010-02-01 17:40:25 -0500

    username should be unique

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,7 @@ class User < ActiveRecord::Base
   validates_acceptance_of :terms_of_service, :on => :create
   validates_length_of :username, :within => 3..13, :on => :create
   validates_presence_of :username
+  validates_uniqueness_of :username
   
   attr_accessible :username, :avatar, :email, :password, :password_confirmation
   attr_readonly   :username
